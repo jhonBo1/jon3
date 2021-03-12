d68e2fbc51290c2d5c52ffa17fcc46a72734768d
jhonBo1 <jonathabqii@gmail.com>
2021-03-12 18:03:40 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,235 @@
+
+<══════════════════════>
+      *JHON💥 BOT*
+ <══════════════════════>
+╭╮╮╱▔▔▔▔╲╭╭╮ 
+ ╰╲╲▏▂╲╱▂▕╱╱╯
+ ┈┈╲▏▇▏▕▇▕╱┈┈
+ ┈┈╱╲▔▕▍▔╱╲┈┈ 
+ ╭╱╱ ▕╋╋╋╋▏╲╲╮
+ ╰╯╯┈╲▂▂╱┈╰╰ ╯
+                         ● ᏴϴͲ●ᎷᎬΝႮ●         
+                         *DONO*    : JHON💥
+
+💥 JHON A LENDA 💥
+┌─────────❶.❷────────
+║〘 INFORMAÇÕES 〙
+║
+╠🛑*JHON💥BOT* 
+╠🛑𝐃𝐎𝐍𝐎: JHON💥💥
+╠🛑 *wa.me/+553599138474*
+╠🛑𝐒𝐓𝐀𝐓𝐔𝐒: ON
+║
+║
+╠══💥〘 MENU 〙💥══
+║
+║☔*.figu*
+║☔*.toimg*
+║☔*.darkjokes (memes aleatórios)*
+║☔*.memeindo*
+║☔*.tts*
+║☔*.lolih [on]*
+║☔*.nsfwloli [off]*
+║☔*.url2img*
+║☔*.leens [na legenda]*
+║☔*.wait [na legenda]*
+║☔*.setprefix*
+║
+╠══💥〘 OUTROS 〙💥══
+║
+║☔*.linkgp*
+║☔*.simih [1/0]*
+║☔*.marcar*
+║☔*.add [@]*
+║☔*.banir [@]*
+║☔*.promover [@]*
+║☔*.rebaixar*
+║☔*.admins*
+║☔*.marcar2*
+║☔*.bc [texto]* (ele faz uma ™)
+║☔*.marcar3*
+║☔*.bloqueados*
+║☔*.bloquear [@]*
+║☔*.desbloquear [@]*
+║☔*.limpar*
+║☔*.bc [ *texto* ]*
+║☔*.bemvindo [1/0]*
+║☔*.clonar [@]*
+║☔*.help1*
+║☔*.dono*
+║☔*.owner*
+║☔*.tts [texto]*
+║☔*.setnome*
+║☔*.termux*
+║☔*.setfoto*
+║☔*.grupoinfo*
+║☔*.ytmp4*
+║☔*.bomdia*
+║☔*.boanoite*
+║☔*.marcar*
+║☔*.marcar2*
+║☔*.marcar3*
+║
+╠══💥〘 IMAGENS 〙💥══
+║
+║☔*.loli* [off]
+║☔*.loli1*
+║☔*.hentai*
+║☔*.dono*
+║☔*.porno*
+║☔*.boanoite*
+║☔*.bomdia*
+║☔*.boatarde*
+║☔*.mia [aleatórias]*
+║☔*.rize [aleatórias]*
+║☔*.minato [aleatórias]*
+║☔*.boruto [aleatórias]*
+║☔*.hinata [aleatórias]*
+║☔*.sasuke [aleatórias]*
+║☔*.sakura [aleatórias]*
+║☔*.naruto [aleatórias]*
+║☔*.meme*   
+║☔*.lofi*
+║☔*.malkova*
+║☔*.canal*
+║☔*.nsfwloli1*
+║☔*.reislin*
+║
+╠══💥〘 INTELIGÊNCIA 〙💥══
+║
+║☔ *.simih 1 (para ativar)*
+║☔ *.simih 0 (para desativar)*
+║ ☔*.simi (sua mensagem)*
+║
+╠══💥〘 EM PRODUÇÃO 〙💥══
+║
+║☔*.gado*
+║☔*.dbz*
+║☔*.gtts loli*
+║☔*.hidegp*
+║☔*.grupos
+║☔*.antinazismo
+║☔*.it 1/0
+║
+╠══💥〘 SÓ PREMIUM 〙💥══
+║
+║☔*.dado*
+║☔*.cekvip*
+║☔*.premiumlist*
+║☔*.delete*
+║☔*.modapk*
+║☔*.indo10*
+║☔*.daftarvip [para virar Premium]*
+║☔*.qrcode*
+║☔*.chentai*
+║☔*.gcpf*
+║☔*.gbin*
+║☔*.pack*
+║☔*.destrava*
+║☔*.gpessoa*
+║
+╠══💥〘 GRUPO 〙💥══
+║
+║☔*.banir*
+║☔*.leveling [on/off]*
+║☔*.level*
+║☔*.add*
+║☔*.promover*
+║☔*.setfoto [na legenda]*
+║☔*.setname [texto]*
+║☔*.rebaixar*
+║☔*.admins*
+║☔*.marcar*
+║☔*.marcar2*
+║☔*.marcar3*
+║☔*.bemvindo [1/0]*
+║☔*.grupoinfo*
+║☔*.bomdia*
+║☔*.boatarde*
+║☔*.boanoite*
+║☔*.setdesc*
+║☔*.bug [sua mensagem]*
+║
+╠══💥〘 ESPECIFICO DO BOT 〙💥══
+║
+║☔*.bug [sua mensagem]*
+║☔*.clonar [@]*
+║☔*.dono*
+║☔*.ping [ver velocidade do bot]*
+║☔*.termux*
+║☔*.gay [@]*
+║☔*.wame*
+║☔*.map (nome)*
+║☔*.setppbot (marque uma img)*
+║☔*.pinterest (nome)*
+║☔*.desligar (so para o dono)*
+║☔*.timer*
+║
+╠══💥〘 MAIS ALGUNS 〙💥══
+║
+║☔*.neko*
+║☔*.ttp [texto]*
+║☔*.testime*
+║☔*.tomp3*
+║☔*.modoanime [on/off]*
+║☔*.modonsfw [on/off]*
+║☔*.happymod [jogo/app]*
+║☔*.rize*
+║☔*.ytsearch*
+║☔*.moddroid [jogo/app]*
+║☔*.xvideos [titulo]**
+║☔*.nomegp*
+║☔*.darkjokes (memes aleatórios)*
+║☔*.animecry*
+║☔*.gay1*
+║☔*.next*
+║☔*.alerta*
+║☔*.belle [img aleatórias]*
+║☔*.pronomeneu [texto]*
+║☔*.hobby*
+║☔*.kiss
+║☔*.analise
+║
+╠══💥〘 COMANDOS DE VOZ 〙💥══
+║
+║ 
+║☔*.ola*
+║☔*.bv*
+║☔*.tchau*
+║☔*.bem*
+║☔*.a*
+║☔*.fdp*
+║☔*.onich*
+║☔*.beat1*
+║☔*.glub*
+║☔*.causs*
+║
+╠══💥〘 OUTROS /2 〙💥══
+║
+║☔ *.antilink [1/0]*
+║☔ *.brainly [pergunta]*
+║☔ *.antiracismo [on/off]*
+║☔ *.setnomebot*
+║☔ *.meme*
+║
+╠══💥〘 INTERATIVOS 〙💥══
+║
+╠══NOTA »
+║Mandar a msg sem o prefixo
+╠════════════════════
+║
+║☔*bah*
+║☔*oii*
+║☔*bv*
+║☔*canta ai bot*
+║☔*grita*
+║☔*causs*
+║☔*gemidao*
+║☔*musica*
+║
+╠══💥〘 JHON💥NO CONTROLE 〙💥══
+║
+║
+║  
+║
+╚═〘 JHON💥 BOT 〙
